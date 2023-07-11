a66f285393d12ec7d5daee5f1746d95bcc79e0fa
prr133f <prr133f@margined.studio>
2023-07-11 20:53:20 +0300

    add check for always positive balance

--- domain/balance.go
+++ domain/balance.go
@@ -3,6 +3,7 @@ package domain
 import (
 	"avito-intership-2022/models"
 	"context"
+	"fmt"
 
 	"github.com/google/uuid"
 )
@@ -38,23 +39,36 @@ func (pg *Postgres) AddBalance(dto models.UserAddBalance) (models.AddBalance, er
 
 func (pg *Postgres) ReserveAmount(dto models.UserReserveAmount) (models.ReserveBalance, error) {
 	var reservedBalance float64
+	var balance float64
 
-	if err := pg.DB.QueryRow(context.Background(), `UPDATE users_schema.user 
-	IF balance - $1 > 0 THEN
-		SET balance = balance - $1, reserved = reserved + $1
-	END IF
-	WHERE id = $2 
-	RETURNING reserved`, dto.ReservedAmount, dto.UserID).Scan(&reservedBalance); err != nil {
+	tx, err := pg.DB.Begin(context.Background())
+	if err != nil {
 		return models.ReserveBalance{}, err
 	}
+	defer tx.Rollback(context.Background())
 
-	if err := pg.DB.QueryRow(context.Background(), `INSERT INTO orders_schema.order(id, date, service_id)
+	if err := tx.QueryRow(context.Background(), `UPDATE users_schema.user 
+	SET balance = balance - $1, reserved = reserved + $1
+	WHERE id = $2 
+	RETURNING balance, reserved`, dto.ReservedAmount, dto.UserID).Scan(&balance, &reservedBalance); err != nil {
+		return models.ReserveBalance{}, err
+	}
+	if balance < 0 {
+		tx.Rollback(context.Background())
+		return models.ReserveBalance{}, fmt.Errorf("balance cannot becomes negative")
+	}
+
+	if err := tx.QueryRow(context.Background(), `INSERT INTO orders_schema.order(id, date, service_id)
 	VALUES($1, DEFAULT, $2)`, dto.OrderID, dto.ServiceID).Scan(); err != nil {
 		return models.ReserveBalance{}, err
 	}
 
-	if err := pg.DB.QueryRow(context.Background(), `INSERT INTO services_schema.service(id, title, amount)
+	if err := tx.QueryRow(context.Background(), `INSERT INTO services_schema.service(id, title, amount)
 	VALUES($1, $2, $3)`, dto.ServiceID, dto.Title, dto.ReservedAmount).Scan(); err != nil {
+		return models.ReserveBalance{}, err
+	}
+
+	if err := tx.Commit(context.Background()); err != nil {
 		return models.ReserveBalance{}, err
 	}
 
@@ -64,11 +78,22 @@ func (pg *Postgres) ReserveAmount(dto models.UserReserveAmount) (models.ReserveB
 func (pg *Postgres) CommitAmount(dto models.UserCommitAmount) (models.ReserveBalance, error) {
 	var reservedBalance float64
 
-	if err := pg.DB.QueryRow(context.Background(), `UPDATE users_schema.user
+	tx, err := pg.DB.Begin(context.Background())
+	if err != nil {
+		return models.ReserveBalance{}, err
+	}
+	defer tx.Rollback(context.Background())
+
+	if err := tx.QueryRow(context.Background(), `UPDATE users_schema.user
 	SET reserved = reserved - $1
 	WHERE id = $2
 	RETURNING reserved`, dto.CommitedAmount, dto.UserID).Scan(&reservedBalance); err != nil {
 		return models.ReserveBalance{}, err
+	}
+
+	if reservedBalance < 0 {
+		tx.Rollback(context.Background())
+		return models.ReserveBalance{}, fmt.Errorf("reserved balance cannot becomes negative")
 	}
 
 	return models.ReserveBalance{ReservedBalance: reservedBalance}, nil
@@ -76,19 +101,35 @@ func (pg *Postgres) CommitAmount(dto models.UserCommitAmount) (models.ReserveBal
 
 func (pg *Postgres) RestoreReservedAmount(dto models.UserRestoreReservedAmount) (models.GetBalance, error) {
 	var currentBalance float64
+	var reservedBalance float64
 
-	if err := pg.DB.QueryRow(context.Background(), `UPDATE users_schema.user 
+	tx, err := pg.DB.Begin(context.Background())
+	if err != nil {
+		return models.GetBalance{}, err
+	}
+	defer tx.Rollback(context.Background())
+
+	if err := tx.QueryRow(context.Background(), `UPDATE users_schema.user 
 	SET reserved = reserved - $1, balance = balance + $1
 	WHERE id = $2
-	RETURNING balance`, dto.RestoredAmount, dto.UserID).Scan(&currentBalance); err != nil {
+	RETURNING balance, reserved`, dto.RestoredAmount, dto.UserID).Scan(&currentBalance, &reservedBalance); err != nil {
 		return models.GetBalance{}, err
 	}
 
-	if err := pg.DB.QueryRow(context.Background(), `DELETE FROM orders_schema.order WHERE id = $1`, dto.OrderID).Scan(); err != nil {
+	if reservedBalance < 0 {
+		tx.Rollback(context.Background())
+		return models.GetBalance{}, fmt.Errorf("reserved balance cannot becomes negative")
+	}
+
+	if err := tx.QueryRow(context.Background(), `DELETE FROM orders_schema.order WHERE id = $1`, dto.OrderID).Scan(); err != nil {
 		return models.GetBalance{}, err
 	}
 
-	if err := pg.DB.QueryRow(context.Background(), `DELETE FROM services_schema.service WHERE id = $1`, dto.ServiceID).Scan(); err != nil {
+	if err := tx.QueryRow(context.Background(), `DELETE FROM services_schema.service WHERE id = $1`, dto.ServiceID).Scan(); err != nil {
+		return models.GetBalance{}, err
+	}
+
+	if err := tx.Commit(context.Background()); err != nil {
 		return models.GetBalance{}, err
 	}
 
